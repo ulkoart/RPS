3f786919244c3ba13f584ce9f219d5e8e636d9f3
Artem Ulko <ulkoart@gmail.com>
2019-12-02 11:30:31 +0300

    game up

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,12 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var robotButton: UIButton!
+    @IBOutlet weak var statuslabel: UILabel!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        resetButton.isHidden = true
     }
-
-
+    
+    func play(_ sign: Sign) {
+        let computerSign = randomSign()
+        robotButton.setTitle(computerSign.emoji, for: .normal)
+        
+        switch sign {
+        case .rock:
+            robotButton.isHidden = false
+            paperButton.isHidden = true
+            scissorsButton.isHidden = true
+        case .paper:
+            robotButton.isHidden = true
+            paperButton.isHidden = false
+            scissorsButton.isHidden = true
+        case .scissors:
+            robotButton.isHidden = true
+            paperButton.isHidden = true
+            scissorsButton.isHidden = false
+        }
+        resetButton.isHidden = false
+        
+        let result = sign.getResult(computerSign)
+        
+        switch result {
+        case .win:
+            statuslabel.text = "It`s a win!"
+            self.view.backgroundColor = UIColor.green
+        case .lose:
+            statuslabel.text = "You lose!"
+            self.view.backgroundColor = UIColor.red
+        case .draw:
+            statuslabel.text = "It`s a draw!"
+            self.view.backgroundColor = UIColor.gray
+        case .start:
+            reset()
+        }
+        
+    }
+    
+    func reset(){
+        statuslabel.text = "Rock, Paper, Scissors?"
+        self.view.backgroundColor = UIColor.white
+        robotButton.setTitle("🤖", for: .normal)
+        resetButton.isHidden = true
+        
+        robotButton.isHidden = false
+        paperButton.isHidden = false
+        scissorsButton.isHidden = false
+    }
+    
+    @IBAction func rockButtonPressed(_ sender: Any) {
+        play(.rock)
+    }
+    
+    @IBAction func paperButtonPressed(_ sender: Any) {
+        play(.paper)
+    }
+    
+    @IBAction func scissorsButtonPressed(_ sender: Any) {
+        play(.scissors)
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        reset()
+    }
 }
 
