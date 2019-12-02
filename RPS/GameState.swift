3764de03463f1f291d63b917a12a737488e03d39
Artem Ulko <ulkoart@gmail.com>
2019-12-02 09:54:53 +0300

    add first part

---
+++ RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by user on 02/12/2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
