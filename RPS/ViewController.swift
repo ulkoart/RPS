6009bf41b6ce374f82a03f786d668f7abecda8fc
Artem Ulko <ulkoart@gmail.com>
2019-12-02 09:24:18 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by user on 02/12/2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
